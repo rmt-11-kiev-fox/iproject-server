11c56ca5e8bba1d2ace276f58531358a84a5854e
mdwikycahyo <55369810+mdwikycahyo@users.noreply.github.com>
2021-06-02 22:11:40 +0700

    apply 3rd party api server

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer')
 const getResult = require('../helpers/getResult')
 const interestResult = require('../helpers/interestResult')
 const shuffle = require('../helpers/shuffle')
+const { default: axios } = require('axios')
 
 class Controller {
   static dashboard(req, res) {
@@ -14,7 +15,7 @@ class Controller {
     const data = req.query
     let finalResults
     let interest_results
-    
+
     if (data.age_group === 'Baby') {
       finalResults = await getResult(Baby, 8)
     } else if (data.age_group === 'Kid') {
@@ -52,12 +53,12 @@ class Controller {
       }
     } else {
       res.status(400).json({
-        message: "Input invalid!"
+        message: 'Input invalid!',
       })
     }
   }
 
-  static sendEmail (req, res) {
+  static sendEmail(req, res) {
     // Generate refresh token : https://developers.google.com/oauthplayground
     var transporter = nodemailer.createTransport({
       pool: true,
@@ -68,30 +69,45 @@ class Controller {
         user: process.env.EMAIL,
         refreshToken: process.env.EMAIL_REFRESH_TOKEN,
         clientId: process.env.EMAIL_CLIENT_ID,
-        clientSecret: process.env.EMAIL_CLIENT_SECRET
+        clientSecret: process.env.EMAIL_CLIENT_SECRET,
       },
       tls: {
-        rejectUnauthorized: false
-      }
+        rejectUnauthorized: false,
+      },
     })
     transporter.verify((error, success) => {
       if (error) {
-        return console.log(error);
+        return console.log(error)
       }
-      transporter.on('token', token => {
-        console.log('User: %s', token.user);
-        console.log('Expires: %s', new Date(token.expires));
+      transporter.on('token', (token) => {
+        console.log('User: %s', token.user)
+        console.log('Expires: %s', new Date(token.expires))
       })
     })
     var mailOptions = {
       from: req.body.email,
       to: process.env.EMAIL,
       subject: `Message about GeeHive from ${req.body.fullname}`,
-      text: req.body.message
+      text: req.body.message,
     }
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) throw res.status(500).json(err.response)
       res.status(201).json('Email sent: ' + info.response)
+    })
+  }
+
+  static exchangeRate(req, res) {
+    axios({
+      url: `https://free.currconv.com/api/v7/convert?q=USD_IDR&compact=ultra&apiKey=${process.env.API_KEY}`,
+      method: 'GET'
+    })
+    .then(response => {
+      res.status(200).json({USD_IDR: response.data.USD_IDR})
+    })
+    .catch(err => {
+      console.log(err);
+      // Invalid API key
+      res.status(400).json(err.response.data.error)
     })
   }
 }
--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ const Controller = require('../controllers/controller')
 const router = express.Router()
 
 router.get('/result', Controller.result)
-
 router.post('/sendEmail', Controller.sendEmail)
+router.get('/exchangeRate', Controller.exchangeRate)
 
 module.exports = router
