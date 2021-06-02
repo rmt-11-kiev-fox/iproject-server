33b137260014df095fe004818873546e16f81cd3
tianmarillio <tiansyah.marillio@gmail.com>
2021-06-02 20:35:04 +0700

    timer done

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,50 +1,82 @@
-// 'use strict'
-// const axios = require('axios')
-// const io = require('../server')
-
+'use strict'
+const axios = require('axios')
+let isActiveServer = false
 // let correct_answer
 // let answers = []
+let currentQuestion = {
+    category: null,
+    question: null,
+    answers: [],
+    correct_answer: null
+}
 
-// class Controller {
-//     static async handle(socket) {
-//         try {
-//             console.log('user connected')
-//             socket.on('submitChat', () => {
-//                 // socket.broadcast.emit('receiveChat')
-//                 axios({
-//                     url: 'http://localhost:3000/questions',
-//                     method: 'GET'
-//                 })
-//                     .then(({ data }) => {
-//                         correct_answer = data.correct_answer
-//                         answers.push
-//                         io.sockets.emit('receiveQuestion', data)
-//                         answer = []
-//                         // socket.broadcast.emit('receiveQuestion', data)
-//                         // socket.emit('receiveQuestion', data)
-//                     })
-//                     .catch(err => {
-//                         console.log(err)
-//                     })
-//             })
-//             // socket.on('getQuestion', () => {
-//             //     axios({
-//             //         url: 'http://localhost:3000/questions',
-//             //         method: 'GET'
-//             //     })
-//             //         .then(({ data }) => {
-//             //             // io.socket.emit('receiveQuestion', data)
-//             //             // socket.broadcast.emit('receiveQuestion', data)
-//             //             // socket.emit('receiveQuestion', data)
-//             //         })
-//             //         .catch(err => {
-//             //             console.log(err)
-//             //         })
-//             // })
-//         } catch (err) {
-//             console.log(err)
-//         }
-//     }
-// }
+let connectedClients = []
 
-// module.exports = Controller
+class Controller {
+    static handle(io) {
+        io.on('connection', socket => {
+            connectedClients.push(socket.id)
+            console.log('USER CONNECTED WITH ID:', socket.id)
+            console.log(connectedClients)
+            socket.on('disconnect', () => {
+                console.log(`USER ${socket.id} DISCONNECTED`)
+                const index = connectedClients.indexOf(socket.id)
+                connectedClients.splice(index, 1)
+                console.log(connectedClients)
+            })
+            socket.on('submitChat', () => {
+                socket.broadcast.emit('receiveChat')
+                // console.log(connectedClients, '<<<<<')
+            })
+            socket.on('getNewQuestion', () => {
+                // isActiveServer = true
+                axios({
+                    url: 'http://localhost:3000/questions',
+                    method: 'GET'
+                })
+                    .then(({ data }) => {
+                        const { category, question, correct_answer } = data
+                        let answers = [
+                            ...data.incorrect_answers,
+                            data.correct_answer
+                        ]
+                        answers = answers.sort((a, b) => a - b)
+                        currentQuestion = {
+                            category,
+                            question,
+                            answers,
+                            correct_answer
+                        }
+                        io.sockets.emit('receiveQuestion', currentQuestion)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            })
+            socket.on('getServerStatus', () => {
+                socket.emit('receiveServerStatus', isActiveServer)
+            })
+            socket.on('getCurrentQuestion', () => {
+                socket.emit('receiveQuestion', currentQuestion)
+            })
+            socket.on('startTrivia', () => {
+                let counter = 10
+                const timer = () => {
+                    console.log(`counter: ${counter}`)
+                    io.sockets.emit('receiveTimeLeft', counter)
+                    counter--
+                    if (!counter) {
+                        clearInterval(interval)
+                        if (connectedClients.length) {
+                            counter = 10
+                            interval = setInterval(timer, 1000)
+                        }
+                    }
+                }
+                let interval = setInterval(timer, 1000)
+            })
+        })
+    }
+}
+
+module.exports = Controller
--- server.js
+++ server.js
@@ -6,59 +6,8 @@ app.set('port', port)
 
 const server = http.createServer(app)
 
-const axios = require('axios')
-
-let isActiveServer = false
-// let correct_answer
-// let answers = []
-let currentQuestion
-// = {
-//     category: null,
-//     question: null,
-//     answers: [],
-//     correct_answer: null
-// }
-
 const io = require('socket.io')(server)
-io.on('connection', socket => {
-    socket.on('submitChat', () => {
-        socket.broadcast.emit('receiveChat')
-    })
-    socket.on('getNewQuestion', () => {
-        isActiveServer = true
-        axios({
-            url: 'http://localhost:3000/questions',
-            method: 'GET'
-        })
-            .then(({ data }) => {
-                const { category, question, correct_answer } = data
-                let answers = [...data.incorrect_answers, data.correct_answer]
-                answers = answers.sort((a, b) => a - b)
-                currentQuestion = {
-                    category,
-                    question,
-                    answers,
-                    correct_answer
-                }
-                io.sockets.emit('receiveQuestion', currentQuestion)
-                // const payload = {
-                //     category,
-                //     question,
-                //     answers: currentQuestion.answers
-                // }
-                // correct_answer = null
-                // answers = []
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    })
-    socket.on('getServerStatus', () => {
-        socket.emit('receiveServerStatus', isActiveServer)
-    })
-    socket.on('getCurrentQuestion', () => {
-        socket.emit('receiveQuestion', currentQuestion)
-    })
-})
+const socketController = require('./controllers/socketController')
+socketController.handle(io)
 
-module.exports = { server, port, io }
+module.exports = { server, port }
