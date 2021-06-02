71d66eadee40ee7d1f3b53b491dff7d29fb79192
mdwikycahyo <55369810+mdwikycahyo@users.noreply.github.com>
2021-06-02 22:16:06 +0700

    Revert "Muhammad Dwiky Cahyo Wicaksono"

--- routers/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const Controller = require('../controllers/controller')
-const router = express.Router()
-
-router.get('/result', Controller.result)
-router.post('/sendEmail', Controller.sendEmail)
-router.get('/exchangeRate', Controller.exchangeRate)
-
-module.exports = router
