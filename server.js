bf4575ccef0cf87e983dc25bd94a0ae931d0586c
tianmarillio <tiansyah.marillio@gmail.com>
2021-06-02 19:05:21 +0700

    get question from 3rd party API

--- server.js
+++ server.js
@@ -1,44 +1,63 @@
 const http = require('http')
 const app = require('./app')
 const port = process.env.PORT || 3000
-const axios = require('axios')
 
 app.set('port', port)
 
 const server = http.createServer(app)
 
-const io = require('socket.io')(server)
-// const socketController = require('./controllers/socketController')
-let correct_answer
-let answers = []
+const axios = require('axios')
 
+let isActiveServer = false
+// let correct_answer
+// let answers = []
+let currentQuestion
+// = {
+//     category: null,
+//     question: null,
+//     answers: [],
+//     correct_answer: null
+// }
+
+const io = require('socket.io')(server)
 io.on('connection', socket => {
     socket.on('submitChat', () => {
         socket.broadcast.emit('receiveChat')
     })
-    socket.on('getQuestion', () => {
+    socket.on('getNewQuestion', () => {
+        isActiveServer = true
         axios({
             url: 'http://localhost:3000/questions',
             method: 'GET'
         })
             .then(({ data }) => {
-                const { category, question } = data
-                correct_answer = data.correct_answer
-                answers = [...data.incorrect_answers, data.correct_answer]
+                const { category, question, correct_answer } = data
+                let answers = [...data.incorrect_answers, data.correct_answer]
                 answers = answers.sort((a, b) => a - b)
-                const payload = {
+                currentQuestion = {
                     category,
                     question,
-                    answers
+                    answers,
+                    correct_answer
                 }
-                // socket.broadcast.emit('receiveQuestion', data)
-                io.sockets.emit('receiveQuestion', payload)
-                correct_answer = null
-                answers = []
+                io.sockets.emit('receiveQuestion', currentQuestion)
+                // const payload = {
+                //     category,
+                //     question,
+                //     answers: currentQuestion.answers
+                // }
+                // correct_answer = null
+                // answers = []
             })
             .catch(err => {
                 console.log(err)
             })
+    })
+    socket.on('getServerStatus', () => {
+        socket.emit('receiveServerStatus', isActiveServer)
+    })
+    socket.on('getCurrentQuestion', () => {
+        socket.emit('receiveQuestion', currentQuestion)
     })
 })
 
