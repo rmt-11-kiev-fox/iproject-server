29fe90e856b7dc95d069d180636dafaf1fac0826
rantoinette <raissa.antoinette@gmail.com>
2021-06-03 11:39:55 +0700

    testing with empty router

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ let userRouter = require("./user");
 let apiRouter = require("./api");
 let watchWishlistRouter = require("./watchWishlist");
 
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
 router.use(userRouter);
 router.use(apiRouter);
 router.use(authentication, watchWishlistRouter);
