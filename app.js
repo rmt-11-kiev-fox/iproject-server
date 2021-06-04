fc3af629bc718a333d6131bb28711920fe29e2f3
achmadwahyupratama <wahyuap12@gmail.com>
2021-06-04 09:20:55 +0700

    socket installed

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ const errorHandler = require('./middlewares/errorHandler')
 const router = require('./routers')
 const app = express()
 const port = process.env.PORT || 3000
+const httpServer = require('http').createServer(app)
+const io = require('socket.io')(httpServer, {})
+io.on('connection', socket => {
+
+})
 app.use(cors({ origin: true, credentials: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(router)
 app.use(errorHandler)
 app.listen(port, ()=>{
+    console.log(`listening from port: ${port}`);
+})
+httpServer.listen(port, () => {
     console.log(`listening from port: ${port}`);
 })
